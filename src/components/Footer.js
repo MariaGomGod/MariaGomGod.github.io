3704b870a8b7b6d1f72bb0cf37e0a37be68edc55
Maria Gomez <maria_gg86@yahoo.es>
2021-09-04 12:09:08 +0200

    añadido navbar en el Header y unificados estilos con respecto al Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,17 @@
-import './Footer.sass';
+import { faFacebook, faTwitter, faPaypal, faCcVisa, faCcMastercard, faCcAmex} from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export default function Footer() {
     return (
         <>
-            <footer role="contentinfo" aria-relevant="removals">
-                <nav id="navbar">
+            <footer>
+                <nav className="navbar">
                     <ul>
                         <li className="nav-item">
                             <a className="nav-link" href="/about">ACERCA&nbsp;DE</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="/empleo">EMPLEO</a>
+                            <a className="nav-link" href="/jobs">EMPLEO</a>
                         </li>
                         <li className="nav-item">
                             <a className="nav-link" href="/term-and-conditions">CONDICIONES&nbsp;DE&nbsp;USO</a>
@@ -23,6 +24,20 @@ export default function Footer() {
                         </li>
                     </ul>
                     <p id="copyright" className="copyright">Copyright&nbsp;{new Date().getFullYear()}&nbsp;&copy;&nbsp;Planet&nbsp;Express</p>
+                    <div id="socialNetworks">
+                        <p>Síguenos en: 
+                            <FontAwesomeIcon icon={faFacebook} />
+                            <FontAwesomeIcon icon={faTwitter} /> 
+                        </p>
+                    </div>
+                    <div id="payments">
+                        <p>Pagos admitidos: 
+                            <FontAwesomeIcon icon={faPaypal} />
+                            <FontAwesomeIcon icon={faCcVisa} />
+                            <FontAwesomeIcon icon={faCcMastercard} />
+                            <FontAwesomeIcon icon={faCcAmex} />
+                        </p>
+                    </div>
                 </nav>
             </footer>
         </>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,39 @@
-import './Header.sass';
-import logo from '../images/logo.png';
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import logo from '../images/logo.png'
 
-export default function Header() {
-
-    return (
-        <div id="header">
-            <a href="/">
-                <img role="button" id="logo" src={logo} alt=""/>
+export default function Header () {
+  return (
+    <header>
+      <div id='header'>
+        <a href='/'>
+          <img role='button' id='logo' src={logo} alt='' />
+        </a>
+      </div>
+      <nav className='navbar'>
+        <ul>
+          <li className='nav-item'>
+            <a className='nav-link' href='/shipments'>
+              Envíos <FontAwesomeIcon icon={faChevronDown} />
             </a>
-        </div>
-    )
+          </li>
+          <li className='nav-item'>
+            <a className='nav-link' href='/parcel-tracking'>
+              Seguimiento <FontAwesomeIcon icon={faChevronDown} />
+            </a>
+          </li>
+          <li className='nav-item'>
+            <a className='nav-link' href='/lockers'>
+              Consignas <FontAwesomeIcon icon={faChevronDown} />
+            </a>
+          </li>
+          <li className='nav-item'>
+            <a className='nav-link' href='/privacy'>
+              Para&nbsp;empresas <FontAwesomeIcon icon={faChevronDown} />
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
 }
