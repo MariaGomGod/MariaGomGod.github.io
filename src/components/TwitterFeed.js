cb24d95096f069a49591482f67ec84b826ed8e24
Maria Gomez <maria_gg86@yahoo.es>
2021-09-05 13:12:41 +0200

    Reemplazado código fuente por versión compilada lista para servidor

--- src/components/TwitterFeed.js
+++
@@ -1,5 +0,0 @@
-export default function TwitterFeed() {
-  return (
-    <a className="twitter-timeline" data-width="300" data-height="1100" href="https://twitter.com/myPlanetExpress?ref_src=twsrc%5Etfw">Tweets by @myPlanetExpress</a>
-  );
-}
